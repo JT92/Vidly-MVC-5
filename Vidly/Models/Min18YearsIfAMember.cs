b32cad391b04c9450c5a23a83722921ff7060b0a
Jack Torres <jacintotorres92@yahoo.com>
2016-11-21 03:40:44 -0500

    Added Customer Validation

---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    /**
+     * Validate Age - Business Rule
+     * Member should be at least 18 years or older to get Monthly, Quarterly, or Annual
+     * memberships. This validation will return success if Unknown or PayAsYouGo is selected, but
+     * return a validation message if another option is selected and age is under 18.
+     * 
+     */
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == MembershipType.Unknown || 
+                customer.MembershipTypeId == MembershipType.PayAsYouGo)
+                return ValidationResult.Success;
+
+            if (customer.Birthdate == null)
+                return new ValidationResult("Birthdate is required.");
+
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+
+            return (age >= 18) 
+                ? ValidationResult.Success 
+                : new ValidationResult("Customer should be at least 18 years old to go on a membership.");
+        }
+    }
+}
