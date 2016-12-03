c541205ac4f5d3e2dc268c7ae4a5bcd3d1c9e0af
Jack Torres <jacintotorres92@yahoo.com>
2016-12-03 03:06:09 -0500

    Build an API for renting movies

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.Models;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        #region DB Context
+
+        private ApplicationDbContext _context;
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        #endregion
+
+        // POST /api/NewRentals
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            foreach (var movie in movies)
+            {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available");
+
+                movie.NumberAvailable--;
+
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+}
